ed0f22ef85144c50f08c4fa7acb888c7806dd93e
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-02-14 01:42:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AK4111/AK_AUTO_FITER.git / AK
+  git clone https://github.com/AK4111/AK_AUTO_FITER.git / AK_AUTO_FITER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AK
+  git clone $UPSTREAM_REPO /AK_AUTO_FITER
 fi
-cd /AK
+cd /AK_AUTO_FITER
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
