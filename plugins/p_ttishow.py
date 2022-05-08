c2052d698912f490c6515cb76bfaf924c4927587
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-05-08 15:18:02 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/Moviez_Express_Official')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/+58SrtDdlynFlZWY1')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
